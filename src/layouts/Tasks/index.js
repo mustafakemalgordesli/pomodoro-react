cef5a59006b5cda5cf471d5dbe62e405b8ea4505
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2022-10-07 19:49:12 +0300

    Tasks branch created

---
+++ src/layouts/Tasks/index.js
@@ -0,0 +1,13 @@
+import "./tasks.scss";
+
+function Tasks() {
+  return (
+    <div className="App-tasks">
+      {/* <button type="button" className="btn btn-outline-danger">
+        <i className="bi bi-trash"></i>
+      </button> */}
+    </div>
+  );
+}
+
+export default Tasks;
