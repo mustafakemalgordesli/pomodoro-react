c554c76e67876a69705e9abb15ef83081b141525
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2022-10-09 21:08:25 +0300

    Task removing feature created

--- src/layouts/Tasks/index.js
+++ src/layouts/Tasks/index.js
@@ -5,25 +5,31 @@ import AddTaskModal from "../../components/AddTaskModal";
 import { v4 as uuidv4 } from "uuid";
 
 function Tasks() {
-  const [tasks, SetTasks] = useState([
-    { id: uuidv4(), value: "Todo 1" },
-    { id: uuidv4(), value: "Todo 2" },
-  ]);
+  const tasklist = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  const [tasks, SetTasks] = useState(tasklist);
 
   const [showModal, SetShowModal] = useState(false);
 
-  const handleRemove = (id) => {};
+  const handleRemove = (id) => {
+    const taskList = tasks.filter((task) => task.id !== id);
+    SetTasks([...taskList]);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+  };
 
   const handleCloseModal = () => {
     SetShowModal(false);
   };
 
   const handleAddTask = (value) => {
-    SetTasks([...tasks, { id: uuidv4(), value }]);
+    const newTasks = { id: uuidv4().toString(), value };
+    const newTaskList = [...tasks, newTasks];
+    SetTasks(newTaskList);
+    localStorage.setItem("tasks", JSON.stringify(newTaskList));
   };
 
   return (
-    <div className="App-tasks container ml-auto mr-auto mt-3 ps-5 pe-5">
+    <div className="App-tasks container ml-auto mr-auto mt-3 ps-5 pe-5 pb-3">
       <h4 className="fw-4 text-white">Tasks</h4>
       <button
         type="button"
@@ -37,7 +43,7 @@ function Tasks() {
 
       <ul className="ms-5 me-5">
         {tasks.map((task) => (
-          <Task task={task} handleRemove={handleRemove} />
+          <Task key={task.id} task={task} handleRemove={handleRemove} />
         ))}
       </ul>
 
